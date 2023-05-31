ed4a1338ee9d6ca627028b8427f90150e24eeb86
minaudts <michielnaudts@hotmail.com>
2023-06-01 00:31:56 +0200

    NavMesh past nu aan wanneer brug spawnt

--- Assets/Scripts/LevelSwitching/AirBridge.cs
+++ Assets/Scripts/LevelSwitching/AirBridge.cs
@@ -1,20 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class AirBridge : MonoBehaviour
 {
-    private BoxCollider _collider;
-    // Start is called before the first frame update
-    void Start()
-    {
-        _collider.enabled = false;
-    }
-
     public void Appear()
     {
         gameObject.SetActive(true);
-        _collider.enabled = true;
         Debug.Log("Bridge appears");
     }
 }
