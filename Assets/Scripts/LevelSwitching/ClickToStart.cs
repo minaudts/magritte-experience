42d266682e5bb4010931ea78d4e586e8733f5296
minaudts <michielnaudts@hotmail.com>
2023-06-11 23:33:08 +0200

    Title screen added

---
+++ Assets/Scripts/LevelSwitching/ClickToStart.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToStart : MonoBehaviour
+{
+    public void StartExperience()
+    {
+        SceneSwitcher.Instance.TransitionToScene(PossibleScenes.MagritteHouse);
+    }
+}
